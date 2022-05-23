a453045b1760df4b337665a6753ffd3fbb264199
YusukeYoshida23 <not_over_yet.1123@hotmail.co.jp>
2022-05-23 22:14:59 +0900

    テスト環境にてsign_in出来ないことへの試行錯誤

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :user do
     name {"Test"}
-    email {"test@example.com"}
+    sequence(:email) { |n| "test#{n}@example.com"}
     password {"test0401"}
     favorite_ball {"ICE STORM"}
   end
